dc922b6d2904b8d4139cb70682e2308af3e00a4a
Kevin Yokley <kyokley2@gmail.com>
2016-07-25 07:38:36 -0500

    Refactoring

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import tempfile
 KEY_LENGTH = 4096
 EXPONENT = 65537
 
+class InvalidSignatureError(Exception):
+    pass
+
 def reseed():
     m2crypto.Rand.rand_seed(os.urandom(1024))
 
@@ -37,15 +40,13 @@ class Agent(object):
         return cipher_text, sig
 
     def decrypt_message(self, message, signer_public_key=None, sig=None):
-        digest = m2crypto.EVP.MessageDigest('sha512')
-        digest.update(message)
         if sig and signer_public_key:
+            digest = m2crypto.EVP.MessageDigest('sha512')
+            digest.update(message)
             if signer_public_key.verify_rsassa_pss(digest.digest(), sig) != 1:
-                print('Signature is invalid!')
-                return
+                raise InvalidSignatureError('Signature is invalid!')
         elif sig or signer_public_key:
-            print('Both sig and public_key are required to verify signature')
-            return
+            raise InvalidSignatureError('Both sig and public_key are required to verify signature')
 
         plain_text = self.key.private_decrypt(message, m2crypto.RSA.pkcs1_oaep_padding)
         return plain_text
@@ -55,13 +56,28 @@ def main():
     alice = Agent()
     bob = Agent()
 
-    message, sig = alice.encrypt_message('This is a secret', bob.pub_key)
-    print message.encode('base64')
+    message = (
+    '''
+    This is a super secret message from Alice to Bob.
+    It should be encrypted and signed appropriately.
+    ''')
+    print('Original Message')
+    print(message)
+    print('Original Message (base64)')
+    print(message.encode('base64'))
 
-    plain_text = bob.decrypt_message(message,
-                                     signer_public_key=alice.pub_key,
-                                     sig=sig)
-    print plain_text
+    cipher_text, sig = alice.encrypt_message(message, bob.pub_key)
+    print('Encrypted (base64)')
+    print(cipher_text.encode('base64'))
+
+    try:
+        plain_text = bob.decrypt_message(cipher_text,
+                                         signer_public_key=alice.pub_key,
+                                         sig=sig)
+    except InvalidSignatureError, e:
+        print(str(e))
+    print('Decrypted Message')
+    print(plain_text)
 
 if __name__ == '__main__':
     main()
